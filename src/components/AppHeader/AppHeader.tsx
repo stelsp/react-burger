e7d30c94e27dd5b7028739538592518b6bfa20a3
stelsp <stelspeh@gmail.com>
2022-01-08 01:56:11 +0300

    feat: add MenuItem-component to AppHeader

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -13,31 +13,52 @@ export default function AppHeader() {
   return (
     <div className={styles.flex}>
       <div className={styles.flex}>
-        <div className={"mt-4 mr-2 mb-4 pt-5 pr-5 pb-5 " + styles.flex}>
-          <BurgerIcon type="primary" />
-          <p className="text text_type_main-default text_color_primary ml-2">
-            Конструктор
-          </p>
-        </div>
+        <MenuItem
+          style={"mt-4 mr-2 mb-4 pt-5 pr-5 pb-5 " + styles.flex}
+          icon={<BurgerIcon type="primary" />}
+          link={"#"}
+          textStyle={
+            "text text_type_main-default text_color_primary ml-2 " + styles.text
+          }
+          text={"Конструктор"}
+        />
 
-        <div className={"mt-4 mb-4 pt-5 pr-5 pb-5 " + styles.flex}>
-          <ListIcon type="secondary" />
-          <p className="text text_type_main-default text_color_inactive ml-2">
-            Лента заказов
-          </p>
-        </div>
+        <MenuItem
+          style={"mt-4 mb-4 pt-5 pr-5 pb-5 " + styles.flex}
+          icon={<ListIcon type="secondary" />}
+          link={"#"}
+          textStyle={
+            "text text_type_main-default text_color_inactive ml-2 " +
+            styles.text
+          }
+          text={"Лента заказов"}
+        />
       </div>
 
       <div className={styles.logo}>
         <Logo />
       </div>
 
-      <div className={"mt-4 mb-4 pt-5 pb-5 pl-5 " + styles.flex}>
-        <ProfileIcon type="secondary" />
-        <p className="text text_type_main-default text_color_inactive ml-2">
-          Личный кабинет
-        </p>
-      </div>
+      <MenuItem
+        style={"mt-4 mb-4 pt-5 pb-5 pl-5 " + styles.flex}
+        icon={<ProfileIcon type="secondary" />}
+        link={"#"}
+        textStyle={
+          "text text_type_main-default text_color_inactive ml-2 " + styles.text
+        }
+        text={"Личный кабинет"}
+      />
+    </div>
+  );
+}
+
+function MenuItem(props: any) {
+  return (
+    <div className={props.style}>
+      {props.icon}
+      <a href={props.link} className={props.textStyle}>
+        {props.text}
+      </a>
     </div>
   );
 }
