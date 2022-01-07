50c804cf8dfb8d42f2c82366caf39c0497ce8f39
stelsp <stelspeh@gmail.com>
2022-01-08 01:35:32 +0300

    feat: add AppHeader draft

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,6 +1,6 @@
 import "../../../node_modules/@ya.praktikum/react-developer-burger-ui-components/dist/ui/common.css";
 import "../../../node_modules/@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css";
-import AppHeaderStyle from "./AppHeader.module.css";
+import styles from "./AppHeader.module.css";
 
 import { Logo } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/logo";
 import {
@@ -11,30 +11,16 @@ import {
 
 export default function AppHeader() {
   return (
-    <div className={AppHeaderStyle.flex}>
-      <div className={AppHeaderStyle.flex}>
-        <div
-          className="mr-2 mt-4 mb-4 pt-5 pr-5 pb-5"
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
+    <div className={styles.flex}>
+      <div className={styles.flex}>
+        <div className={"mt-4 mr-2 mb-4 pt-5 pr-5 pb-5 " + styles.flex}>
           <BurgerIcon type="primary" />
           <p className="text text_type_main-default text_color_primary ml-2">
             Конструктор
           </p>
         </div>
 
-        <div
-          className="mt-4 mb-4 pt-5 pr-5 pb-5"
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
+        <div className={"mt-4 mb-4 pt-5 pr-5 pb-5 " + styles.flex}>
           <ListIcon type="secondary" />
           <p className="text text_type_main-default text_color_inactive ml-2">
             Лента заказов
@@ -42,18 +28,11 @@ export default function AppHeader() {
         </div>
       </div>
 
-      <div className={AppHeaderStyle.logo}>
+      <div className={styles.logo}>
         <Logo />
       </div>
 
-      <div
-        className="mt-4 mb-4 pt-5 pl-5 pb-5"
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
+      <div className={"mt-4 mb-4 pt-5 pb-5 pl-5 " + styles.flex}>
         <ProfileIcon type="secondary" />
         <p className="text text_type_main-default text_color_inactive ml-2">
           Личный кабинет
