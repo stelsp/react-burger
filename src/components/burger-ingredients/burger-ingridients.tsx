03f7abd420c59b8a49037810a839147b9c5db1ab
stelsp <stelspeh@gmail.com>
2022-01-17 15:39:23 +0300

    f

--- src/components/burger-ingredients/burger-ingridients.tsx
+++ src/components/burger-ingredients/burger-ingridients.tsx
@@ -27,7 +27,7 @@ export default function BurgerIngredients({ data }: any) {
           Начинки
         </Tab>
       </div>
-      <div className={styles.list + " custom-scroll"}>
+      <div className={" custom-scroll " + styles.list}>
         <h3 className={titleStyle}>Булки</h3>
         <ul className={styles.cards}>
           {data.map((el: any) => {
