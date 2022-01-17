7b36a9d4b86207e4a24705b2a43b8886b363a9e3
stelsp <stelspeh@gmail.com>
2022-01-18 00:46:00 +0300

    f

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/app/app";
+import ModalOverlay from "./components/modal/modal";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ModalOverlay>
+      <Modal></Modal>
+    </ModalOverlay>
+  </React.StrictMode>,
+  document.getElementById("modal-root")
 );
