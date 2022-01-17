49d12fe3d7c7ac16d08ef840c6b7ae997ec2ceaf
stelsp <stelspeh@gmail.com>
2022-01-17 19:53:24 +0300

    f

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./app.module.css";
 
@@ -7,7 +7,21 @@ import BurgerIngredients from "../burger-ingredients/burger-ingridients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import data from "../../utils/data";
 
+let url = "https://norma.nomoreparties.space/api/ingredients";
+
+const getData = () =>
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      let info = res.data;
+      return info;
+    });
+
+getData();
+
 export default function App() {
+  const [state, setState] = useState([]);
+
   return (
     <div className={styles.app}>
       <AppHeader />
