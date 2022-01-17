94833913f96865d0eb8e13ce97aef780a96acb5a
stelsp <stelspeh@gmail.com>
2022-01-17 20:29:33 +0300

    feat: connect API data

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import styles from "./app.module.css";
 
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingridients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
-import data from "../../utils/data";
 
 let url = "https://norma.nomoreparties.space/api/ingredients";
 
-const getData = () =>
-  fetch(url)
-    .then((res) => res.json())
-    .then((res) => {
-      let info = res.data;
-      return info;
-    });
-
-getData();
-
 export default function App() {
-  const [state, setState] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.data);
+      });
+  }, []);
 
   return (
     <div className={styles.app}>
