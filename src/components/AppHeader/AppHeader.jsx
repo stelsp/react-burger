70a677e963201831623d6649d5bc7bfa813c20ff
stelsp <stelspeh@gmail.com>
2022-04-16 00:54:15 +0300

    Update AppHeader.jsx

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,5 +1,4 @@
 import styles from "./AppHeader.module.css";
-import styled from "styled-components";
 import MenuItem from "./MenuItem/MenuItem";
 import { Logo } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/logo";
 import {
@@ -11,35 +10,12 @@ import { APP_HEADER_LINKS } from "../../constants/content";
 
 import { Link } from "react-router-dom";
 
-const Header = styled.header`
-  background: var(--background);
-  width: 100%;
-  margin: 0 auto;
-`;
-
-const Wrapper = styled.div`
-  width: 1240px;
-  margin: 0 auto;
-`;
-
-const Nav = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
 function AppHeader() {
   return (
-    <Header>
-      <Wrapper>
-        <Nav>
-          <Container>
+    <header className={styles.header}>
+      <div className={styles.wrapper}>
+        <nav className={styles.flex}>
+          <div className={styles.flex}>
             <MenuItem
               linkStyle={styles.link + " pr-5 mr-2"}
               textStyle={"ml-2"}
@@ -55,14 +31,13 @@ function AppHeader() {
               to={"/ingredients/60d3b41abdacab0026a733c7"}
               text={APP_HEADER_LINKS.ORDER_FEED}
             />
-          </Container>
+          </div>
 
           <div className={styles.logo}>
             <Link to="/">
               <Logo />
             </Link>
           </div>
-
           <MenuItem
             linkStyle={styles.link + " pl-5"}
             textStyle={"ml-2"}
@@ -70,9 +45,9 @@ function AppHeader() {
             to={"/profile"}
             text={APP_HEADER_LINKS.ACCOUNT}
           />
-        </Nav>
-      </Wrapper>
-    </Header>
+        </nav>
+      </div>
+    </header>
   );
 }
 
