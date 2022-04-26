2eee3c09b345577e6f4e188a0a6c5c285124039d
stelsp <stelspeh@gmail.com>
2022-04-26 12:18:01 +0300

    f

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -1,7 +1,56 @@
 import styles from "./Feed.module.css";
+import feed from "./FeedInfo.module.css";
 import ProfileOrders from "../../components/ProfileOrders/ProfileOrders";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
+
+const FeedInfo = () => {
+  return (
+    <div className={feed.container}>
+      <div className={feed.orders}>
+        <div className={feed.done}>
+          <h3 className={feed.title}>Готовы:</h3>
+          <ul className={feed.list}>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+            <li className={feed.doneItem}>034533</li>
+          </ul>
+        </div>
+        <div className={feed.pending}>
+          <h3 className={feed.title}>В работе:</h3>
+          <ul className={feed.list}>
+            <li className={feed.pendingItem}>034533</li>
+            <li className={feed.pendingItem}>034533</li>
+            <li className={feed.pendingItem}>034533</li>
+            <li className={feed.pendingItem}>034533</li>
+          </ul>
+        </div>
+      </div>
+      <div className={feed.all}>
+        <p className={feed.title}>Выполнено за все время:</p>
+        <h2 className={feed.number}>28 752</h2>
+      </div>
+      <div className={feed.today}>
+        <p className={feed.title}>Выполнено за сегодня:</p>
+        <h2 className={feed.number}>138</h2>
+      </div>
+    </div>
+  );
+};
 
 function Feed() {
   const dispatch = useDispatch();
@@ -19,7 +68,9 @@ function Feed() {
         </div>
       </div>
       <div className={styles.container}>
-        <div className={styles.statusContainer}>Feed</div>
+        <div className={styles.statusContainer}>
+          <FeedInfo />
+        </div>
       </div>
     </div>
   );
