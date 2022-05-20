f264671dcd49bc2afa40940f60488e27858b8bff
stelsp <stelspeh@gmail.com>
2022-05-20 20:49:50 +0300

    feat: /constants js to ts

---
+++ src/constants/api-url.ts
@@ -0,0 +1,12 @@
+export const API_URL =
+  "https://norma.nomoreparties.space/api" as "https://norma.nomoreparties.space/api";
+export const URL_KEY_INGREDIENTS = "/ingredients" as "/ingredients";
+export const URL_KEY_ORDERS = "/orders" as "/orders";
+export const URL_KEY_PASSWORD_FORGOT = "/password-reset" as "/password-reset";
+export const URL_KEY_PASSWORD_RESET =
+  "/password-reset/reset" as "/password-reset/reset";
+export const URL_KEY_REGISTER = "/auth/register" as "/auth/register";
+export const URL_KEY_LOGIN = "/auth/login" as "/auth/login";
+export const URL_KEY_USER = "/auth/user" as "/auth/user";
+export const URL_KEY_TOKEN = "/auth/token" as "/auth/token";
+export const URL_KEY_LOGOUT = "/auth/logout" as "/auth/logout";
